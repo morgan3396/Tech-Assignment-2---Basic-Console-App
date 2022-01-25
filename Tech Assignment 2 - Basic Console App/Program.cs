e9db8c3a11aba2fae2a50a96f70b5afd9965eed1
Morgan Harmon <morgan35@usf.edu>
2022-01-25 18:32:54 -0500

    Add project files.

---
+++ Tech Assignment 2 - Basic Console App/Program.cs
@@ -0,0 +1,14 @@
+﻿// Programmer: Morgan Harmon
+
+using System;
+
+namespace Tech_Assignment_2___Basic_Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
